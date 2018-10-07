fa953db5d6942e0c1edf1dc554b19eb9853abda8
Jazz <jazziesf21@yahoo.com>
2018-10-07 15:57:22 -0700

    all test complete

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,4 +1,24 @@
 # Creates a new array to return the intersection of the two input arrays
 def intersection(array1, array2)
-  raise NotImplementedError
+
+  b = 0
+  section = []
+  if (array1 == nil || array1 == []) || (array2 == nil || array2 == [])
+    return section
+  end
+
+  while array1 != nil && array2 != nil
+    i = 0
+    until array1[i] == nil
+      if array2[b] == nil
+        return section
+      elsif array1[i] == array2[b]
+        section << array1[i]
+      end
+      i += 1
+    end
+    b += 1
+  end
+
+
 end
